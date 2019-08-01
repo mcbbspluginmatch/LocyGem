3a2cb0368069fdca1542ad871a5840c6e6a16637
D_xiaox <1838115958@qq.com>
2019-08-01 12:39:15 +0800

    来自星空的评语

--- src/main/java/com/locydragon/gem/core/gems/InlayListener.java
+++ src/main/java/com/locydragon/gem/core/gems/InlayListener.java
@@ -26,6 +26,7 @@ public class InlayListener implements Listener {
 		if (handIn != null && handIn.getType() != Material.BARRIER && handIn.hasItemMeta() && handIn.getItemMeta().hasLore()) {
 			GemItem gemItem = GemItem.forItem(zero);
 			if (ItemStackBuilder.equalsInSet(handIn, GemItems.ignoreArmor, EqualsInfo.ABOUT_LORE, EqualsInfo.ABOUT_NAME)) {
+				// 既然同一个方法使用辣么多次 那就写个方法呀 这样不是更加方便 —— 星空
 				// 以下的方法中相同片段重复出现多次，为什么不提取出来作为一个方法 —— 754503921
 				if (zero == null || gemItem.getHoles() == 0) {
 					e.getPlayer().sendMessage(LocyGem.out + "很抱歉,这个物品无法镶嵌!o(╥﹏╥)o");
